7088f90010ba40057e2becb25506f388c97381a5
MarcosCM01 <40147926+MarcosCM01@users.noreply.github.com>
2020-02-17 11:30:14 -0600

    Analizando primer palabra del documento

--- Proyecto_LFA/Helpers.cs
+++ Proyecto_LFA/Helpers.cs
@@ -10,25 +10,87 @@ namespace Proyecto_LFA
 {
     public class Helpers
     {
-        public static bool ArchivoVacio(string direccion) 
+        public static char[] reservada_SETS = { 'S', 'E', 'T', 'S'};//OPCIONAL QUE EXISTA
+        public static char[] reservada_TOKENS = { 'T', 'O', 'K', 'E', 'N', 'S' };//TIENE QUE EXISTIR
+        public static bool ArchivoVacio(string direccion)// verifica que el aechivo este vacio
         {
             FileInfo propiedades = new FileInfo(direccion);
             if (propiedades.Length >0)
             {
-                return true;
+               return true;
             }
             return false;
         }
         public static void LeerArchivo(string rutaFile) 
         {
+            //variables auxiliares
             var line = string.Empty;
+            var no_lineaError = 0;
+            var no_columnaError = 1;
+            var bandera_Inicio = false;
+            var letra_Inicio = ' ';
             using (StreamReader reader = new StreamReader(rutaFile))
             {
                 while ((line = reader.ReadLine()) != null)
                 {
-                    MessageBox.Show(line);
+                    no_lineaError++;
+                   
+                    var linea = line.ToCharArray();// linea es cada linea del documento
+
+                    //ANALIZO LA PRIMER PALABRA---> que SETS y TOKENS esten correctamente escritas
+                    if (linea.Length > 0 && bandera_Inicio == false)
+                    {
+                        bandera_Inicio = true;
+                        switch (linea[0])
+                        {
+                            case 'S':
+                                if (Analizador_Reservada(reservada_SETS, linea, 1, ref no_columnaError) != true)
+                                { //si encuentra el error, me saca
+                                    MostrarError(no_lineaError, no_columnaError);
+                                }
+                                letra_Inicio = 'S';
+                                break;
+                            case 'T':
+                                if (Analizador_Reservada(reservada_TOKENS, linea, 1, ref no_columnaError) != true)
+                                {
+                                    MostrarError(no_lineaError, no_columnaError);
+                                }
+                                letra_Inicio = 'T';
+                                break;
+                        }
+                    }
+
+                    //if (linea.Length > 0 && bandera_Inicio == true)
+                    //{
+
+                    //}
+                    
                 }
             }
+        }
+        public static bool Analizador_Reservada(char[] reservada, char[] linea, int contador, ref int no_columnaError) 
+        {
+            if (contador < reservada.Length)
+            {
+                no_columnaError++;
+                if (linea[contador] == reservada[contador])
+                {
+                    return Analizador_Reservada(reservada, linea, contador + 1, ref no_columnaError);
+                }
+            }
+            if (contador == reservada.Length)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static void MostrarError(int no_Linea, int no_columnaError) //CASO NO SE CUMPLIO LA GRAMATICA
+        {
+            MessageBox.Show("ERROR EN EL ARCHIVO CARGADO, en la linea "+no_Linea +", columna " +no_columnaError);//AGREGAR no_Columna
         }
     }
 }
